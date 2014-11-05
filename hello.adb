5e6e7d7a13bcb0d20bfea9768dc01aabeede527a
Naveen Mysore <navimn1991@gmail.com>
2014-11-05 00:59:54 +0000

    clock1.2

--- hello.adb
+++ hello.adb
@@ -1,21 +1,21 @@
 with Ada.Text_IO; use Ada.Text_IO; 
 procedure Hello is
-X : Integer; 
-begin
-X := 0;
-Put(ASCII.ESC & "[2J");
-While_Loop: 
-while X <=5 loop
-delay Duration(2.0);
-Put_Line ("Tray 1:");
-Put_Line("Tray 2:");
-Put_Line("Tray 3:");
-Put_Line("Tray 4:");
-Put_Line("      ");
-Put_Line("      ");
-Put_Line("Reserve:");
-delay Duration(2.0); 
-Put(ASCII.ESC & "[2J");
-X := X + 1;
-end loop While_Loop;
+	X : Integer; 
+	begin
+		X := 0;
+		Put(ASCII.ESC & "[2J");
+		While_Loop: 
+			while X <=5 loop
+			delay Duration(2.0);
+			Put_Line ("Tray 1:");
+			Put_Line("Tray 2:");
+			Put_Line("Tray 3:");
+			Put_Line("Tray 4:");
+			Put_Line("      ");
+			Put_Line("      ");
+			Put_Line("Reserve:");
+			delay Duration(2.0); 
+			Put(ASCII.ESC & "[2J");
+			X := X + 1;
+		end loop While_Loop;
 end Hello;
