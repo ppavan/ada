637dbb98846d360a486d822dd822ee698e5d83b4
Naveen Mysore <navimn1991@gmail.com>
2014-11-12 21:26:30 +0000

    transfer_to_five_min

--- clock/mclock.adb
+++ clock/mclock.adb
@@ -15,15 +15,8 @@ procedure Mclock is
    	Tray_fifteen_min : Fifo_Type;-- third tray size 3
  	Tray_one_hour : Fifo_Type;-- fourth tray size 11
    	Tray_collector : Fifo_Type;-- this is a reserve tray size 21 (4+2+3+11+1(trigger))
-  	-- flags
-	Tray_one_min_status : Boolean;-- 0 empty 1 mean full
-	Tray_five_min_status: Boolean;-- 0 empty 1 means full
-	Tray_fifteen_min_status : Boolean;-- 0 means empty 1 means full
-	Tray_one_hour_status : Boolean;-- 0 means empty 1 means full
-	Tray_collector_status : Boolean;-- 
-	-- temporary variables
+  	-- temporary variables
 	Val : Integer;
-	Temp : Integer;
 	ptemp : Integer;
 	H : Integer;
 	l : Integer;
@@ -33,21 +26,33 @@ procedure Mclock is
 	size_of_fifteen_min : Integer;
 	size_of_one_hour : Integer;
 	size_of_collector : Integer;
+	-- size variables
+	max_size_of_one_min : Integer;
+	max_size_of_five_min : Integer;
+	max_size_of_fifteen_min : Integer;
+	max_size_of_one_hour : Integer;
+	max_size_of_collector : Integer;
 	Now : Time := Clock;
 	-- functions
 	function push_to_one_min(status: Integer) return Integer is
 	begin
-		Pop(Tray_collector, Val);
-		Push(Tray_one_min,Val);
-		Tray_one_min_status := TRUE;
-		size_of_one_min := size_of_one_min + 1;
-		size_of_collector := size_of_collector - 1;
+		if size_of_one_min < max_size_of_one_min then	
+			Pop(Tray_collector, Val);--when one min tray is full we have not yet poped collector
+			Push(Tray_one_min,Val);
+			size_of_one_min := size_of_one_min + 1;
+			size_of_collector := size_of_collector - 1;
+		else
+			Pop(Tray_one_min, Val);
+			Push(Tray_five_min, Val);
+			size_of_five_min := size_of_five_min + 1;	
+		end if;
 		return status;
 	end;
 	function push_to_five_min(status: Integer) return Integer is
 	begin
-		--pop from min list and push here and pop a null from here and push to 
-		--collector
+		Pop(Tray_one_min, Val);
+		Push(Tray_five_min, Val);
+		size_of_five_min := size_of_one_min + 1;
 	return status;
 	end;
 	-- Initial tray setup start -----------------------------------
@@ -65,53 +70,43 @@ procedure Mclock is
 		-------------
 		--tray one min
 		Put("Tray A (one min):     ");
-		if Tray_one_min_status then
-			for X in 1..size_of_one_min loop
-      				Pop(Tray_one_min, ptemp);
-      				Put(Integer'Image(ptemp));
-				Push(Tray_one_min, ptemp);
-   			end loop;
-		end if;
+		for X in 1..size_of_one_min loop
+      			Pop(Tray_one_min, ptemp);
+      			Put(Integer'Image(ptemp));
+			Push(Tray_one_min, ptemp);
+   		end loop;
 		New_Line;
 		--tray five min
-		Put("Tray B (five min):    ");
-		if Tray_five_min_status then	
-			for X in 1..size_of_five_min loop
-      				Pop(Tray_five_min, Val);
-      				Put(Integer'Image(Val));
-				Push(Tray_five_min, Val);
-   			end loop;
-		end if;
+		Put("Tray B (five min):    ");	
+		for X in 1..size_of_five_min loop
+      			Pop(Tray_five_min, Val);
+      			Put(Integer'Image(Val));
+			Push(Tray_five_min, Val);
+   		end loop;
 		New_Line;
 		--tray fifteen min
 		Put("Tray C (fifteen min): ");
-		if Tray_fifteen_min_status then
-			for X in 1..size_of_fifteen_min loop
-      				Pop(Tray_fifteen_min, Val);
-      				Put(Integer'Image(Val));
-				Push(Tray_fifteen_min, Val);
-   			end loop;
-		end if;
+		for X in 1..size_of_fifteen_min loop
+      			Pop(Tray_fifteen_min, Val);
+      			Put(Integer'Image(Val));
+			Push(Tray_fifteen_min, Val);
+   		end loop;
 		New_Line;
 		--tray one hour
 		Put("Tray D (One Hour):    ");
-		if Tray_one_hour_status then
-			for X in 1..size_of_one_hour loop
-      				Pop(Tray_one_hour, Val);
-      				Put(Integer'Image(Val));
-				Push(Tray_one_hour, Val);
-   			end loop;
-		end if;
+		for X in 1..size_of_one_hour loop
+      			Pop(Tray_one_hour, Val);
+      			Put(Integer'Image(Val));
+			Push(Tray_one_hour, Val);
+   		end loop;
 		New_Line;
 		--tray collector
 		Put("Tray E (Collector):   ");
-		if Tray_collector_status then
-			for X in 1..size_of_collector loop
-      				Pop(Tray_collector, Val);
-      				Put(Integer'Image(Val));
-				Push(Tray_collector, Val);
-   			end loop;
-		end if;
+		for X in 1..size_of_collector loop
+      			Pop(Tray_collector, Val);
+      			Put(Integer'Image(Val));
+			Push(Tray_collector, Val);
+   		end loop;
 		return status;
 		exception
 			when Constraint_Error =>
@@ -134,16 +129,16 @@ begin	-- main here
 	size_of_fifteen_min := 0;
 	size_of_one_hour := 0;
 	size_of_collector := 21;
-	-------------
-	Tray_one_min_status := FALSE;-- 0 empty 1 mean full
-	Tray_five_min_status:= FALSE;-- 0 empty 1 means full
-	Tray_fifteen_min_status := FALSE;-- 0 means empty 1 means full
-	Tray_one_hour_status := FALSE;-- 0 means empty 1 means full
-	Tray_collector_status := TRUE; 
+	-- size variables
+	max_size_of_one_min := 4;
+	max_size_of_five_min := 2;
+	max_size_of_fifteen_min := 3;
+	max_size_of_one_hour := 11;
+	max_size_of_collector := 21;
 	--------------
 	H := clear_screen(1);
 	l := 0;
-	for I in 1..4 loop
+	for I in 1..6 loop
 		Put_line(Image(Date => Now, Time_Zone => -7*60));
 		H := print_trays(1);
 		delay Duration(1.0);--print and wait for a sec
